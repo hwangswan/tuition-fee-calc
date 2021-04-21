0bf6985f46f34393ca2780af748e256de47ab41d
Hwang S. Wan <mrthedzam@gmail.com>
2021-04-21 07:14:32 +0700

    Added documentation for Helper.java

--- src/Helper.java
+++ src/Helper.java
@@ -11,11 +11,24 @@ import java.io.*;
 
 import Semester.*;
 
+/**
+ * This class provides some helper functions
+ * in order to help developers output to a CSV file / input from a CSV file.
+ *
+ * @author trhgquan - https://github.com/trhgquan
+ */
 public class Helper {
   /**
-   * This class helps input data.
+   * This class is all about input.
+   *
    */
   public static class Input {
+    /**
+     * This method return a Semester.Course object from a CSV line.
+     *
+     * @param  String
+     * @return Semester.Course
+     */
     public static Semester.Course scanCourse(String inputLine) {
       String[] currentLineData = inputLine.split(",", 3);
       
@@ -24,6 +37,12 @@ public class Helper {
       return course;
     }
 
+    /**
+     * This method return a list of Semester.Course read from a CSV file.
+     *
+     * @param  String
+     * @return Semester.Course[]
+     */
     public static Semester.Course[] scanCourseList(String inputFileName) {
       List<Semester.Course> builder = new ArrayList<Semester.Course>();
 
@@ -32,7 +51,6 @@ public class Helper {
         Scanner scanner = new Scanner(reader);
 
         while (scanner.hasNextLine()) {
-          // Get 3 field only.
           Semester.Course newCourse = scanCourse(scanner.nextLine());
 
           builder.add(newCourse);
@@ -41,12 +59,19 @@ public class Helper {
         scanner.close();
       } catch (FileNotFoundException e) {
         System.out.println("Error happened!");
+
         e.printStackTrace();
       }
 
       return builder.toArray(new Semester.Course[0]);
     }
 
+    /**
+     * This method return a Semester scanned from a CSV file.
+     *
+     * @param  String
+     * @return Semester
+     */
     public static Semester scanSemester(String fileName) {
       Semester.Course[] courseList = scanCourseList(fileName);
 
@@ -57,21 +82,32 @@ public class Helper {
   }
   
   /**
-   * This class helps output data.
+   * This class is all about output.
+   *
    */
-  public static class Output { 
+  public static class Output {
+    /**
+     * This method print the Semester to a CSV file.
+     *
+     * @param  String
+     * @param  Semester
+     */
     public static void toFile(String outputFileName, Semester semester) {
       Semester.Course[] courseList = semester.courseList();
       
       try {
         FileWriter writer = new FileWriter(outputFileName);
+
+        // Print the file's header.
         writer.write("Course name,Credits,Fee\n");
 
+        // Print each course in that semester.
         for (int i = 0; i < courseList.length; ++i) {
           writer.write(courseList[i].toCSVString());
           writer.write("\n");
         }
 
+        // Print the course's total tuition fee.
         writer.write(semester.toCSVString());
         
         writer.close();
@@ -79,6 +115,7 @@ public class Helper {
         System.out.println(outputFileName + " created!");
       } catch (IOException e) {
         System.out.println("Error happened!");
+
         e.printStackTrace();
       }
     } 
