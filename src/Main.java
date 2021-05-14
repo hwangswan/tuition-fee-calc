3b76b7f521c9540889b956375b5d5cdc506cbda0
Hwang S. Wan <mrthedzam@gmail.com>
2021-05-14 14:52:02 +0700

    Added comments

--- src/Main.java
+++ src/Main.java
@@ -15,21 +15,26 @@ class Main {
    */
   public static void main(String args[]) {
     try {
+      // Get files from parameter.
       String inputFile  = args[0];
       String outputFile = args[1];
 
+      // Create a new Semester.
       Semester newSemester = Helper.Input.scanSemester(inputFile);
 
+      // Set class cost from Config.
       newSemester.setClassCost(Integer.parseInt(
                                       Helper.Config
                                       .getInstance()
                                       .load("tuition-fee-calc.class")));
 
+      // Set lab cost from Config.
       newSemester.setLabCost(Integer.parseInt(
                                     Helper.Config
                                     .getInstance()
                                     .load("tuition-fee-calc.lab")));
 
+      // Print Semester cost to file.
       Helper.Output.toFile(outputFile, newSemester);
     }
 
